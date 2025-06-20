a001fdabe275f65619f340f210a50c42d8ed5fc7
Oluwaseyi <hassanabdullahioluwaseyi@gmail.com>
2025-06-20 13:20:27 +0100

    Added clickaway component

--- src/components/CustomSelect.tsx
+++ src/components/CustomSelect.tsx
@@ -2,6 +2,7 @@ import { Dispatch, FC, SetStateAction, useState } from "react";
 import { useTheme } from "../context/ThemeContext";
 import { IoIosArrowDown } from "react-icons/io";
 import useAnimationCleanup from "../hooks/useAnimationCleanup";
+import ClickAway from "./ClickAway";
 
 type CustomSelectProps = {
   options:
@@ -56,54 +57,56 @@ const CustomSelect: FC<CustomSelectProps> = ({
   };
 
   return (
-    <div className="relative w-full ">
-      <button
-        className={`${
-          theme === "dark" ? "bg-dark/70" : "bg-light"
-        } w-full px-3 rounded-full py-1 cursor-pointer flex justify-between items-center`}
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        <span>{selectedOption.label}</span>
-        <IoIosArrowDown
-          className={`${isOpen ? "rotate-180" : ""} transition duration-500`}
-        />
-      </button>
-      {isVisible && (
-        <div
-          className={`${theme === "dark" ? "bg-dark" : "bg-light"} ${
-            isOpen
-              ? "animate-dropdown opacity-100"
-              : "animate-dropdown-reverse opacity-0 pointer-events-none"
-          } py-1 rounded-lg w-full absolute flex flex-col top-[35px] overflow-y-scroll h-fit max-h-[500px] z-30 scroll_bar`}
+    <ClickAway onClickAway={() => setIsOpen(false)}>
+      <div className="relative w-full ">
+        <button
+          className={`${
+            theme === "dark" ? "bg-dark/70" : "bg-light"
+          } w-full px-3 rounded-full py-1 cursor-pointer flex justify-between items-center`}
+          onClick={() => setIsOpen(!isOpen)}
         >
-          {options
-            ?.sort((a, b) => b.label.localeCompare(a.label))
-            .map((option) => (
-              <div
-                key={option.value}
-                className={`cursor-pointer px-3 py-1 ${
-                  theme === "dark"
-                    ? "hover:bg-dark-bg/80"
-                    : "hover:bg-light-bg/80"
-                } ${
-                  selectedOption.value === option.value
-                    ? "font-bold bg-gray-600/20"
-                    : ""
-                }`}
-                onClick={() => handleClick(+option.value)}
-                tabIndex={0}
-                onKeyDown={(e) => {
-                  if (e.key === "Enter") {
-                    handleClick(+option.value);
-                  }
-                }}
-              >
-                {option.label}
-              </div>
-            ))}
-        </div>
-      )}
-    </div>
+          <span>{selectedOption.label}</span>
+          <IoIosArrowDown
+            className={`${isOpen ? "rotate-180" : ""} transition duration-500`}
+          />
+        </button>
+        {isVisible && (
+          <div
+            className={`${theme === "dark" ? "bg-dark" : "bg-light"} ${
+              isOpen
+                ? "animate-dropdown opacity-100"
+                : "animate-dropdown-reverse opacity-0 pointer-events-none"
+            } py-1 rounded-lg w-full absolute flex flex-col top-[35px] overflow-y-scroll h-fit max-h-[500px] z-30 scroll_bar`}
+          >
+            {options
+              ?.sort((a, b) => b.label.localeCompare(a.label))
+              .map((option) => (
+                <div
+                  key={option.value}
+                  className={`cursor-pointer px-3 py-1 ${
+                    theme === "dark"
+                      ? "hover:bg-dark-bg/80"
+                      : "hover:bg-light-bg/80"
+                  } ${
+                    selectedOption.value === option.value
+                      ? "font-bold bg-gray-600/20"
+                      : ""
+                  }`}
+                  onClick={() => handleClick(+option.value)}
+                  tabIndex={0}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                      handleClick(+option.value);
+                    }
+                  }}
+                >
+                  {option.label}
+                </div>
+              ))}
+          </div>
+        )}
+      </div>
+    </ClickAway>
   );
 };
 
