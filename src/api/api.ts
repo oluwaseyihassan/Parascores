b767dbebe5ca3f21940e425285d48e6f534ac3ec
Oluwaseyi <hassanabdullahioluwaseyi@gmail.com>
2025-08-11 18:19:47 +0100

    Added base url to env

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 // const API_KEY = import.meta.env.VITE_API_KEY;
+const baseUrl = import.meta.env.VITE_BASE_URL
 
 const api = axios.create({
-    // baseURL: "http://localhost:2000/api",
-    baseURL: "https://sportmonksapi.onrender.com/api",
+    baseURL: baseUrl,
     headers: {
         "Content-Type": "application/json",
         // "X-API-KEY": API_KEY
